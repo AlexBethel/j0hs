f2bc9bdd0f75abbc53f9d8dfabab554aa5c7a695
Alex Bethel <abethel@cs.nmt.edu>
2023-01-02 20:39:42 -0600

    Add missing type defs to arguments.java

--- testfiles/arguments.java
+++ testfiles/arguments.java
@@ -9,3 +9,12 @@ public class Main {
         return i + 1;
     }
 }
+
+public class int {
+    public int __add__(int other) extern int_add;
+    public void print() extern int_print;
+}
+
+public class boolean {
+    public boolean __not__() extern boolean_not;
+}
